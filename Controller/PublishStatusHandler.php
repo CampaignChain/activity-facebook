54740ac22760643ee91814e1e25419b10fd7e340
Sandro Groganz <sandro@campaignchain.com>
2015-10-07 15:46:27 +0200

    CE-382 Adjust all modules to work with new modules framework

--- Controller/PublishStatusHandler.php
+++ Controller/PublishStatusHandler.php
@@ -10,9 +10,10 @@
 
 namespace CampaignChain\Activity\FacebookBundle\Controller;
 
-use CampaignChain\CoreBundle\Controller\Module\AbstractActivityModuleHandler;
-use CampaignChain\CoreBundle\Entity\Activity;
+use CampaignChain\CoreBundle\Controller\Module\AbstractActivityHandler;
+use Symfony\Component\Form\Form;
 use CampaignChain\CoreBundle\Entity\Location;
+use CampaignChain\Operation\FacebookBundle\Job\PublishStatus;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\TwigBundle\TwigEngine;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -23,64 +24,63 @@ use CampaignChain\Operation\FacebookBundle\Entity\PageStatus;
 use CampaignChain\Operation\FacebookBundle\Entity\UserStatus;
 use CampaignChain\Operation\FacebookBundle\EntityService\Status;
 
-class PublishStatusHandler extends AbstractActivityModuleHandler
+class PublishStatusHandler extends AbstractActivityHandler
 {
     const DATETIME_FORMAT_TWITTER = 'F j, Y';
 
-    protected $detailService;
-    protected $restClient;
     protected $em;
+    protected $contentService;
+    protected $job;
     protected $session;
     protected $templating;
 
     public function __construct(
         EntityManager $em,
-        Status $detailService,
+        Status $contentService,
+        PublishStatus $job,
         $session,
         TwigEngine $templating
     )
     {
-        $this->detailService = $detailService;
         $this->em = $em;
+        $this->contentService = $contentService;
+        $this->job = $job;
         $this->session = $session;
         $this->templating = $templating;
     }
 
-    public function getOperationDetail(Location $location, Operation $operation = null)
+    public function createContent(Location $location)
     {
-        /*
-         * If no Operation has been defined, this means that we're creating a
-         * new entry.
-         */
-        if(!$operation) {
-            // Check whether the status will be published on a User or Page stream.
-            $facebookLocation = $this->em
-                ->getRepository('CampaignChainLocationFacebookBundle:LocationBase')
-                ->findOneByLocation($location);
+        // Check whether the status will be published on a User or Page stream.
+        $facebookLocation = $this->em
+            ->getRepository('CampaignChainLocationFacebookBundle:LocationBase')
+            ->findOneByLocation($location);
 
-            if (!$facebookLocation) {
-                throw new \Exception(
-                    'No Facebook location found.'
-                );
-            }
-
-            if ($facebookLocation instanceof User) {
-                $status = new UserStatus();
-            } elseif ($facebookLocation instanceof Page) {
-                $status = new PageStatus();
-            }
-
-            return $status->setFacebookLocation($facebookLocation);
-        } else {
-            return $this->detailService->getStatusByOperation($operation);
+        if (!$facebookLocation) {
+            throw new \Exception(
+                'No Facebook location found.'
+            );
         }
+
+        if ($facebookLocation instanceof User) {
+            $status = new UserStatus();
+        } elseif ($facebookLocation instanceof Page) {
+            $status = new PageStatus();
+        }
+
+        return $status->setFacebookLocation($facebookLocation);
     }
 
-    public function processOperationDetails(Operation $operation, $data)
+    public function getContent(Location $location, Operation $operation = null)
+    {
+        return $this->contentService->getStatusByOperation($operation);
+    }
+
+    public function processContent(Operation $operation, $data)
     {
         try {
             // If the status has already been created, we modify its data.
-            $status = $this->detailService->getStatusByOperation($operation);
+            $status = $this->contentService->getStatusByOperation($operation);
             $status->setMessage($data['message']);
             $class = get_class($status);
             if (strpos($class, 'CampaignChain\Operation\FacebookBundle\Entity\UserStatus') !== false) {
@@ -94,9 +94,21 @@ class PublishStatusHandler extends AbstractActivityModuleHandler
         return $status;
     }
 
-    public function readOperationDetailsAction(Operation $operation)
+    public function postPersistNewEvent(Operation $operation, Form $form, $content = null)
     {
-        $status = $this->detailService->getStatusByOperation($operation);
+        // Content to be published immediately?
+        $this->publishNow($operation, $form);
+    }
+
+    public function postPersistEditEvent(Operation $operation, Form $form, $content = null)
+    {
+        // Content to be published immediately?
+        $this->publishNow($operation, $form);
+    }
+
+    public function readAction(Operation $operation)
+    {
+        $status = $this->contentService->getStatusByOperation($operation);
 
         $statusType = 'user';
 
@@ -130,5 +142,21 @@ class PublishStatusHandler extends AbstractActivityModuleHandler
                 'activity' => $operation->getActivity(),
                 'status_type' => $statusType,
             ));
+    }
+
+    private function publishNow(Operation $operation, Form $form)
+    {
+        if ($form->get('campaignchain_hook_campaignchain_due')->has('execution_choice') && $form->get('campaignchain_hook_campaignchain_due')->get('execution_choice')->getData() == 'now') {
+            $this->job->execute($operation->getId());
+            $content = $this->contentService->getStatusByOperation($operation);
+            $this->session->getFlashBag()->add(
+                'success',
+                'The status was published. <a href="'.$content->getUrl().'">View it on Facebook</a>.'
+            );
+
+            return true;
+        }
+
+        return false;
     }
 }
