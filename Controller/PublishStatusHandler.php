af4c272b8f8dd723fae924661f37785e812c7303
Sandro Groganz <sandro@campaignchain.com>
2016-09-30 12:50:53 +0200

    CampaignChain/campaignchain#223 Deal with identical content in campaigns

--- Controller/PublishStatusHandler.php
+++ Controller/PublishStatusHandler.php
@@ -163,7 +163,7 @@ class PublishStatusHandler extends AbstractActivityHandler
     {
         if ($form->get('campaignchain_hook_campaignchain_due')->has('execution_choice') && $form->get('campaignchain_hook_campaignchain_due')->get('execution_choice')->getData() == 'now') {
             // Validate whether we can execute the Activity?
-            $isExecutable = $this->validator->isExecutableByChannel(
+            $isExecutable = $this->validator->isExecutableByLocation(
                 $this->contentService->getContent($operation), new \DateTime()
             );
             if(!$isExecutable['status']) {
